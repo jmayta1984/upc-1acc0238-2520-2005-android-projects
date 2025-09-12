99ebf1661efbd7afce86c5c405b5ac5478331cb2
jmayta1984 <jmayta@pucp.edu.pe>
2025-09-12 10:26:24 -0500

    chore: initial project setup

---
+++ EasyShop/app/src/main/java/pe/edu/upc/easyshop/Home.kt
@@ -0,0 +1,280 @@
+package pe.edu.upc.easyshop
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.FilterList
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material.icons.outlined.ShoppingCart
+import androidx.compose.material3.Card
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import pe.edu.upc.easyshop.ui.theme.EasyShopTheme
+
+@Composable
+fun Home() {
+
+    val categories = listOf(
+        Category.All,
+        Category.Men,
+        Category.Women,
+        Category.Girls,
+        Category.Boys
+    )
+
+    val selectedCategory = remember {
+        mutableStateOf<Category>(Category.All)
+    }
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(64.dp)
+                .padding(horizontal = 8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(48.dp)
+                    .clip(RoundedCornerShape(16.dp))
+                    .background(MaterialTheme.colorScheme.primary)
+            ) {
+                Icon(
+                    Icons.Default.Person,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(8.dp),
+                    tint = MaterialTheme.colorScheme.onPrimary
+                )
+            }
+            CustomSpacer()
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    "Hello Alex",
+                )
+                Text(
+                    "Good morning!",
+                    style = MaterialTheme.typography.titleMedium,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            CustomSpacer()
+            RoundedIcon(Icons.Outlined.Notifications)
+            CustomSpacer()
+            RoundedIcon(Icons.Outlined.ShoppingCart)
+
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(64.dp)
+                .padding(horizontal = 8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                leadingIcon = {
+                    Icon(
+                        Icons.Default.Search,
+                        contentDescription = null
+                    )
+                },
+                placeholder = {
+                    Text("Search")
+                },
+                shape = RoundedCornerShape(16.dp),
+                modifier = Modifier.weight(1f)
+            )
+            CustomSpacer()
+            RoundedIcon(Icons.Outlined.FilterList)
+
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(64.dp)
+                .padding(horizontal = 8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                "Categories",
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.SemiBold,
+                modifier = Modifier.weight(1f)
+            )
+            TextButton(onClick = {}) { Text("See all") }
+        }
+
+        LazyRow {
+            items(categories) { category ->
+                FilterChip(
+                    selected = category == selectedCategory.value,
+                    onClick = {
+                        selectedCategory.value = category
+                    },
+                    label = {
+                        Text(
+                            category.label,
+                            modifier = Modifier.padding(vertical = 12.dp)
+                        )
+                    },
+                    modifier = Modifier.padding(8.dp),
+                    shape = RoundedCornerShape(16.dp)
+                )
+            }
+        }
+        Box(
+            modifier = Modifier
+                .height(192.dp)
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(16.dp))
+                    .background(
+                        brush = Brush.linearGradient(
+                            listOf(
+                                MaterialTheme.colorScheme.primary,
+                                MaterialTheme.colorScheme.inversePrimary
+                            )
+                        )
+                    )
+            ) {
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxHeight()
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        "Get your special sale up to 40%",
+                        style = MaterialTheme.typography.headlineSmall,
+                        color = MaterialTheme.colorScheme.onPrimary,
+                        fontWeight = FontWeight.SemiBold
+                    )
+                    ElevatedButton(onClick = {}) {
+                        Text("Shop now")
+                    }
+                }
+                Image(
+                    painterResource(R.drawable.banner),
+                    contentDescription = null,
+                    modifier = Modifier.weight(1f),
+                    contentScale = ContentScale.FillWidth
+                )
+            }
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(64.dp)
+                .padding(horizontal = 8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                "Popular",
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.SemiBold,
+                modifier = Modifier.weight(1f)
+            )
+            TextButton(onClick = {}) { Text("See all") }
+        }
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2)
+        ) {
+            items(categories){ category ->
+
+
+            }
+        }
+    }
+}
+
+@Composable
+fun CustomSpacer() {
+    Spacer(modifier = Modifier.width(8.dp))
+}
+
+
+@Composable
+fun RoundedIcon(icon: ImageVector) {
+    Box(
+        modifier = Modifier
+            .size(48.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .background(MaterialTheme.colorScheme.surfaceContainer)
+    ) {
+        Icon(
+            icon,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp),
+
+            )
+    }
+}
+
+sealed class Category(val label: String) {
+    object All : Category("All")
+    object Men : Category("Men")
+    object Women : Category("Women")
+    object Boys : Category("Boys")
+    object Girls : Category("Girls")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomePreview() {
+    EasyShopTheme {
+        Home()
+    }
+
+}
---
+++ EasyShop/app/src/main/java/pe/edu/upc/easyshop/Login.kt
@@ -0,0 +1,132 @@
+package pe.edu.upc.easyshop
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import pe.edu.upc.easyshop.ui.theme.EasyShopTheme
+
+@Composable
+fun Login(onLogin: () -> Unit) {
+    val email = remember {
+        mutableStateOf("")
+    }
+
+    var password by remember {
+        mutableStateOf("")
+    }
+
+    val isVisible = remember {
+        mutableStateOf(false)
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        OutlinedTextField(
+            value = email.value,
+            onValueChange = {
+                email.value = it
+            },
+            placeholder = {
+                Text(text = "Email")
+            },
+            leadingIcon = {
+                Icon(
+                    Icons.Default.Email,
+                    contentDescription = null
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        )
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = {
+                password = it
+            },
+            placeholder = {
+                Text(text = "Password")
+            },
+            leadingIcon = {
+                Icon(
+                    Icons.Default.Lock,
+                    contentDescription = null
+                )
+            },
+            trailingIcon = {
+                IconButton(
+                    onClick = {
+                        isVisible.value = !isVisible.value
+                    }
+                ) {
+                    Icon(
+                        if (isVisible.value) {
+                            Icons.Default.Visibility
+                        } else {
+                            Icons.Default.VisibilityOff
+                        },
+                        contentDescription = null
+                    )
+                }
+            },
+            visualTransformation = if (isVisible.value) {
+                VisualTransformation.None
+            } else {
+                PasswordVisualTransformation()
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+
+        )
+
+        Button(
+            onClick = onLogin,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+
+        ) {
+            Text("Login")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun LoginPreview() {
+    EasyShopTheme {
+        Login {
+
+        }
+    }
+
+}
